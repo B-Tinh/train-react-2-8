ee60922f8c8f58928021cb3e2591d35e00a3d2ca
Bach Ngoc Tinh <winanh2151993@gmail.com>
2019-08-02 16:27:08 +0700

    react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Login from './layout/login';
+import Profile from './layout/profile';
+import Register from './layout/register';
+import { Router, Route} from 'react-router-dom';
+import history from './history';
+
+class App extends Component {
+	
+	render(){
+  return (        
+       //  <Router history={history}>
+       // 			<Route path ="/"  exact component ={Login} />
+		    	// <Route path ="/profile" component ={Profile} />
+       //  </Router>  
+       <Register />
+
+  );
+}
+}
+
+export default App;
